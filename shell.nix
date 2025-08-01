02d6eac241360f4471ff8513f45c9d8e33677318
James ‘Twey’ Kay <twey@twey.co.uk>
2025-08-01 16:09:36 +0100

    use `nix run` instead of `stack`

--- shell.nix
+++ shell.nix
@@ -1,9 +1,1 @@
 (builtins.getFlake ("git+file://" + toString ./.)).devShells.${builtins.currentSystem}.default
-mkShell {
-  buildInputs = [
-    gnumake ghc haskellPackages.hakyll asciidoctor graphviz-nox
-    (python3.withPackages (ps: [ps.fontforge])) ttfautohint-nox imagemagick
-    pikchr nodePackages.mermaid-cli
-    bashInteractive
-  ];
-}
