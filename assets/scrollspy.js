10dac4e664f9441c6452537ef5944f187a87ccee
James ‘Twey’ Kay <twey@twey.co.uk>
2025-12-27 22:31:12 +0000

    remove Bootstrap and its scrollspy

---
+++ assets/scrollspy.js
@@ -0,0 +1,31 @@
+const navItems = document.querySelectorAll("body > nav ul li > a:first-child");
+const sectionVisibilities = {};
+
+const observer = new IntersectionObserver(
+  (entries, observer) => {
+    for (const entry of entries)
+      sectionVisibilities[entry.target.id] = entry.isIntersecting;
+
+    let currentSectionId;
+    for (const [id, visible] of Object.entries(sectionVisibilities))
+      if (visible) {
+        currentSectionId = id;
+        break;
+      }
+
+    for (const navItem of navItems)
+      navItem.className =
+        navItem.getAttribute("href") === `#${currentSectionId}`
+          ? "active"
+          : "";
+  },
+  {
+    root: null,
+    rootMargin: "0px",
+    threshold: 0,
+  },
+);
+
+for (const section of document.querySelectorAll("article > section")) {
+  if ("id" in section) observer.observe(section);
+}
